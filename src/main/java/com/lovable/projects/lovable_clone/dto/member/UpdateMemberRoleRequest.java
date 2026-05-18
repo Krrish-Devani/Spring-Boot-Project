375fe184b642983f2ffa53b3c39fbbaff058bf51
Krrish-Devani <krrishdevani84@gmail.com>
2026-05-18 20:19:59 +0530

    Project Member Contoller Routes, DTO, Service interface

---
+++ src/main/java/com/lovable/projects/lovable_clone/dto/member/UpdateMemberRoleRequest.java
@@ -0,0 +1,6 @@
+package com.lovable.projects.lovable_clone.dto.member;
+
+import com.lovable.projects.lovable_clone.enums.ProjectRole;
+
+public record UpdateMemberRoleRequest(ProjectRole role) {
+}
