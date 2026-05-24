28cfc361823c6ffec5f2570f22ef9a5abf92c0c8
Krrish-Devani <krrishdevani84@gmail.com>
2026-05-24 18:41:02 +0530

    Create Project and MapStruct Implementation

--- src/main/java/com/lovable/projects/lovable_clone/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/lovable/projects/lovable_clone/service/impl/ProjectServiceImpl.java
@@ -3,13 +3,45 @@ package com.lovable.projects.lovable_clone.service.impl;
 import com.lovable.projects.lovable_clone.dto.project.ProjectRequest;
 import com.lovable.projects.lovable_clone.dto.project.ProjectResponse;
 import com.lovable.projects.lovable_clone.dto.project.ProjectSummaryResponse;
+import com.lovable.projects.lovable_clone.entity.Project;
+import com.lovable.projects.lovable_clone.entity.User;
+import com.lovable.projects.lovable_clone.mapper.ProjectMapper;
+import com.lovable.projects.lovable_clone.repository.ProjectRepository;
+import com.lovable.projects.lovable_clone.repository.UserRepository;
 import com.lovable.projects.lovable_clone.service.ProjectService;
+import jakarta.transaction.Transactional;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@Transactional
 public class ProjectServiceImpl implements ProjectService {
+
+    ProjectRepository projectRepository;
+    UserRepository userRepository;
+    ProjectMapper projectMapper;
+
+    @Override
+    public ProjectResponse createProject(ProjectRequest request, Long userId) {
+
+        User owner = userRepository.findById(userId).orElseThrow();
+
+        Project project = Project.builder()
+                .name(request.name())
+                .owner(owner)
+                .isPublic(false)
+                .build();
+
+        project = projectRepository.save(project);
+        return projectMapper.toProjectResponse(project);
+    }
+
     @Override
     public List<ProjectSummaryResponse> getUserProjects(Long userId) {
         return List.of();
@@ -17,11 +49,6 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public ProjectResponse getUserProjectById(Long id, Long userId) {
-        return null;
-    }
-
-    @Override
-    public ProjectResponse createProject(ProjectRequest request, Long userId) {
         return null;
     }
 
