dd48503fd94cd4da781710443eb04121f218f39c
Krrish-Devani <krrishdevani84@gmail.com>
2026-06-15 15:44:44 +0530

    Updated the Query of findAllAccessibleByUser and added findAccessibleProjectById

--- src/main/java/com/lovable/projects/lovable_clone/repository/ProjectRepository.java
+++ src/main/java/com/lovable/projects/lovable_clone/repository/ProjectRepository.java
@@ -7,18 +7,32 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     @Query("""
-            SELECT p FROM Project p
-            WHERE p.deletedAt IS NULL
-            AND p.owner.id = :userId
+            SELECT p as project, pm.projectRole as role
+            FROM Project p
+            JOIN ProjectMember pm ON pm.project.id = p.id
+            WHERE pm.user.id = :userId
+              AND p.deletedAt IS NULL
             ORDER BY p.updatedAt DESC
-            """
-    )
-
+            """)
     List<Project> findAllAccessibleByUser(@Param("userId") Long userId);
+
+    @Query("""
+            SELECT p FROM Project p
+            WHERE p.id = :projectId
+                AND p.deletedAt IS NULL
+                AND EXISTS (
+                    SELECT 1 FROM ProjectMember pm
+                    WHERE pm.id.userId = :userId
+                    AND pm.id.projectId = :projectId
+                )
+            """)
+    Optional<Project> findAccessibleProjectById(@Param("projectId") Long projectId,
+                                                @Param("userId") Long userId);
 
 }
