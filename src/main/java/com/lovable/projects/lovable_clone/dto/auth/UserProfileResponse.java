27f696be83472d4be131f00d0bf79386221028ef
Krrish-Devani <krrishdevani84@gmail.com>
2026-06-20 23:24:44 +0530

    Signup Flow Implementation

--- src/main/java/com/lovable/projects/lovable_clone/dto/auth/UserProfileResponse.java
+++ src/main/java/com/lovable/projects/lovable_clone/dto/auth/UserProfileResponse.java
@@ -2,7 +2,7 @@ package com.lovable.projects.lovable_clone.dto.auth;
 
 public record UserProfileResponse(
         Long id,
-        String email,
+        String username,
         String name
 ) {
 }
