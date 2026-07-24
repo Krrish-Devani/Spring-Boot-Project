18780f3907b463f713ac1cdb70d264f747cd8e9e
Krrish-Devani <krrishdevani84@gmail.com>
2026-07-24 22:43:37 +0530

    Added SecurityExpressions for Managing the Permissions

---
+++ src/main/java/com/lovable/projects/lovable_clone/security/SecurityExpressions.java
@@ -0,0 +1,46 @@
+package com.lovable.projects.lovable_clone.security;
+
+import com.lovable.projects.lovable_clone.enums.ProjectPermission;
+import com.lovable.projects.lovable_clone.repository.ProjectMemberRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Component;
+
+@Component("security")
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class SecurityExpressions {
+
+    ProjectMemberRepository projectMemberRepository;
+    AuthUtil authUtil;
+
+    private boolean hasPermission(Long projectId, ProjectPermission projectPermission) {
+        Long userId = authUtil.getCurrentUserId();
+
+        return projectMemberRepository.findRoleByProjectIdAndUserId(projectId, userId).
+                map(role -> role.getPermissions().contains(projectPermission))
+                .orElse(false);
+    }
+
+    public boolean canViewProject(Long projectId) {
+        return hasPermission(projectId, ProjectPermission.VIEW);
+    }
+
+    public boolean canEditProject(Long projectId) {
+        return hasPermission(projectId, ProjectPermission.EDIT);
+    }
+
+    public boolean canDeleteProject(Long projectId) {
+        return hasPermission(projectId, ProjectPermission.DELETE);
+    }
+
+    public boolean canViewMembers(Long projectId) {
+        return hasPermission(projectId, ProjectPermission.VIEW_MEMBERS);
+    }
+
+    public boolean canManageMembers(Long projectId) {
+        return hasPermission(projectId, ProjectPermission.MANAGE_MEMBERS);
+    }
+
+}
