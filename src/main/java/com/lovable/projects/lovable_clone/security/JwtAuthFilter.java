b4f34bfe6ef5db12c9c2c898c28b3fd2e37eb771
Krrish-Devani <krrishdevani84@gmail.com>
2026-08-05 06:22:59 +0530

    Fixes the Isuues in Resspective Classes

--- src/main/java/com/lovable/projects/lovable_clone/security/JwtAuthFilter.java
+++ src/main/java/com/lovable/projects/lovable_clone/security/JwtAuthFilter.java
@@ -4,7 +4,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,6 +19,7 @@ import java.io.IOException;
 @Component
 @Slf4j
 @RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     AuthUtil authUtil;
