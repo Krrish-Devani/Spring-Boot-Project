48c3e5c9f714f0e798040bf7ec12aa59ff3e548e
Krrish-Devani <krrishdevani84@gmail.com>
2026-08-20 22:54:51 +0530

    Created Ai Generation Service Implementation

---
+++ src/main/java/com/lovable/projects/lovable_clone/service/impl/AiGenerationServiceImpl.java
@@ -0,0 +1,14 @@
+package com.lovable.projects.lovable_clone.service.impl;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class AiGenerationServiceImpl {
+}
