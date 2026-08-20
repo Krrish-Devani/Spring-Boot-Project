e2f95ef80b1cdc080f2ba7c55e295d394c494c83
Krrish-Devani <krrishdevani84@gmail.com>
2026-08-20 22:51:22 +0530

    Created Ai Generation Service and Stream Response in chat dto

---
+++ src/main/java/com/lovable/projects/lovable_clone/dto/chat/StreamResponse.java
@@ -0,0 +1,3 @@
+package com.lovable.projects.lovable_clone.dto.chat;
+
+public record StreamResponse(String text) {}
---
+++ src/main/java/com/lovable/projects/lovable_clone/service/AiGenerationService.java
@@ -0,0 +1,8 @@
+package com.lovable.projects.lovable_clone.service;
+
+import com.lovable.projects.lovable_clone.dto.chat.StreamResponse;
+import reactor.core.publisher.Flux;
+
+public interface AiGenerationService {
+    Flux<StreamResponse> streamResponse(String message, Long projectId);
+}
